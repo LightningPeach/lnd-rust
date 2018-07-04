aadc8a0dce35fe78ff31c9773dff9f6fb9533386
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-07-04 23:55:59 +0300

    added macaroon support

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,35 +2,49 @@ extern crate futures;
 extern crate futures_cpupool;
 extern crate protobuf;
 extern crate grpc;
+extern crate bytes;
 
 extern crate lnd_rust;
 
 use lnd_rust::rpc_grpc::Lightning;
 use lnd_rust::TLSCertificate;
+use lnd_rust::MacaroonData;
 
 fn main() {
     use std::net::{SocketAddr, IpAddr, Ipv4Addr};
 
     println!("lnd-rust main");
 
-    let default_path = "/home/twenty/work/ln-ico/simple-simnet/bitcoin-bitcoind/rpc/rpc.cert";
-    let cert_filename = std::env::args()
-        .into_iter().skip(1).next()
-        .unwrap_or(default_path.to_owned());
-    let certificate = TLSCertificate::from_der_path(cert_filename)
-        .unwrap();
+    let certificate = {
+        let default_path = "/home/twenty/work/ln-ico/simple-simnet/bitcoin-bitcoind/rpc/rpc.cert";
+        let cert_filename = std::env::args()
+            .into_iter().skip(1).next()
+            .unwrap_or(default_path.to_owned());
+        TLSCertificate::from_der_path(cert_filename)
+            .unwrap()
+    };
 
-    let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 10009);
+    let client = {
+        let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 10009);
+        let grpc_client = certificate.create_client(&socket_addr, "localhost", Default::default());
+        lnd_rust::rpc_grpc::LightningClient::with_client(grpc_client)
+    };
 
-    let grpc_client = certificate.create_client(&socket_addr, "localhost", Default::default());
-    let client = lnd_rust::rpc_grpc::LightningClient::with_client(grpc_client);
+    let macaroon_data = {
+        let default_path = "tools/data/lnd1/admin.macaroon";
+        let macaroon_file_path = std::env::args()
+            .into_iter().skip(2).next()
+            .unwrap_or(default_path.to_owned());
+        MacaroonData::from_file_path(macaroon_file_path)
+            .unwrap()
+    };
 
     let req = lnd_rust::rpc::GetInfoRequest::new();
-    let resp = client.get_info(grpc::RequestOptions::new(), req);
+    let resp = client.get_info(macaroon_data.metadata(), req);
     println!("{:?}", resp.wait());
 
     let wallet_req = lnd_rust::rpc::WalletBalanceRequest::new();
-    let wallet_resp = client.wallet_balance(grpc::RequestOptions::new(), wallet_req);
+    let wallet_resp = client.wallet_balance(macaroon_data.metadata(), wallet_req);
     let w = wallet_resp.wait().unwrap().1;
     println!("{:?}\n", w);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate grpc;
 extern crate tls_api;
 extern crate tls_api_native_tls;
 extern crate httpbis;
+extern crate bytes;
 
 pub mod rpc;
 pub mod rpc_grpc;
@@ -65,5 +66,41 @@ impl TLSCertificate {
         let tls = httpbis::ClientTlsOption::Tls(host.to_owned(), Arc::new(connector));
         grpc::Client::new_expl(&socket_addr, host, tls, conf)
             .unwrap()
+    }
+}
+
+pub struct MacaroonData {
+    raw: Vec<u8>,
+}
+
+impl MacaroonData {
+    pub fn from_file_path<P: AsRef<Path>>(path: P) -> IOResult<Self> {
+        use std::io::{Error, ErrorKind};
+
+        let output = Command::new("xxd")
+            .args(&["-ps", "-u", "-c", "1000"])
+            .arg(path.as_ref())
+            .output()?;
+
+        if !output.status.success() {
+            let error = String::from_utf8_lossy(&output.stderr);
+            Err(Error::new(ErrorKind::InvalidInput, error.as_ref()))
+        } else {
+            let mut data = output.stdout;
+            data.retain(|&z|
+                ((z >= '0' as _) && (z <= '9' as _)) |
+                    ((z >= 'A' as _) && (z <= 'F' as _))
+            );
+            Ok(MacaroonData {
+                raw: data,
+            })
+        }
+    }
+
+    pub fn metadata(&self) -> grpc::RequestOptions {
+        let macaroon = bytes::Bytes::from(self.raw.clone());
+        let mut options = grpc::RequestOptions::new();
+        options.metadata.add(grpc::MetadataKey::from("macaroon"), macaroon);
+        options
     }
 }
