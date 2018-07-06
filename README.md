cddb14428405e562a9c2a1a33db27a40ceb67152
Vladislav Melnik <vladislav.melnik@bitfury.com>
2018-07-06 15:16:54 +0300

    added documentation, fixed authors field in manifest

--- README.md
+++ README.md
@@ -5,3 +5,39 @@ The rust-lnd is the rust binding to the Lightning Network Daemon.
 ## Requirements
 
 Go and lnd is required to generate the additional code and build the package.
+
+## Updating
+
+The file `protos/rpc.proto` should be synced with `https://github.com/lightningnetwork/lnd/blob/master/lnrpc/rpc.proto`.
+
+## Usage
+
+First, add the following to `Cargo.toml`:
+
+```toml
+[dependencies]
+rust-lnd = "0.1"
+grpc = "0.5.0"
+```
+
+Next, create lightning client object:
+
+```rs
+let lightning_client = {
+    let certificate = lnd_rust::TLSCertificate::from_der_path("path/to/file.crt").unwrap();
+    let config = Default::default();
+    let socket = "127.0.0.1:9000".parse().unwrap();
+    let grpc_client = certificate.create_client(&socket, "localhost", config);
+    lnd_rust::rpc_grpc::LightningClient::with_client(grpc_client)
+};
+```
+
+Now, it is possible to make requests:
+
+```rs
+let request = lnd_rust::rpc::GetInfoRequest::new();
+let options = grpc::RequestOptions::new();
+let response = lightning_client.get_info(options, request);
+```
+
+See `src/bin/main.rs` for details.
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,14 @@
-extern crate futures;
-extern crate futures_cpupool;
-extern crate protobuf;
 extern crate grpc;
-extern crate bytes;
-
 extern crate lnd_rust;
 
+use lnd_rust::rpc_grpc::LightningClient;
 use lnd_rust::rpc_grpc::Lightning;
 use lnd_rust::TLSCertificate;
 use lnd_rust::MacaroonData;
+
+use lnd_rust::rpc;
+
+use grpc::RequestOptions;
 
 fn main() {
     use std::net::SocketAddr;
@@ -44,17 +44,17 @@ fn main() {
         let socket_addr: SocketAddr = socket_addr_string.parse().unwrap();
         let host = socket_addr.ip().to_string();
         let grpc_client = certificate.create_client(&socket_addr, host.as_str(), Default::default());
-        lnd_rust::rpc_grpc::LightningClient::with_client(grpc_client)
+        LightningClient::with_client(grpc_client)
     };
 
     let metadata = |macaroon_data: &MacaroonData|
-        grpc::RequestOptions { metadata: macaroon_data.metadata(), };
+        RequestOptions { metadata: macaroon_data.metadata(), };
 
-    let req = lnd_rust::rpc::GetInfoRequest::new();
+    let req = rpc::GetInfoRequest::new();
     let resp = client.get_info(metadata(&macaroon_data), req);
     println!("{:?}", resp.wait());
 
-    let wallet_req = lnd_rust::rpc::WalletBalanceRequest::new();
+    let wallet_req = rpc::WalletBalanceRequest::new();
     let wallet_resp = client.wallet_balance(metadata(&macaroon_data), wallet_req);
     let w = wallet_resp.wait().unwrap().1;
     println!("{:?}\n", w);
